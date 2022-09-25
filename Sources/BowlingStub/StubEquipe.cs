6a7c8c5d4fbc6efd0ed14450a2059b79913b9cb8
AUGUSTIN_100 <augustinaffognon2000@gmail.com>
2022-09-25 20:20:32 +0200

    Stub Joueur et Equipe

---
+++ Sources/BowlingStub/StubEquipe.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class StubEquipe
+{
+	private List<Equipe> listEquipes = new List<Equipe>();
+	public StubEquipe()
+	{
+	}
+
+	public List<Equipe> ListEquipes(int n = 10, int j = 2)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			listEquipes.Add(new Equipe() { Nom = "Equipe " + i + 1 });
+
+			for(int k = 0; k < j; k++)
+            {
+				listEquipes.ElementAt(i).AjouterJoueur(new Joueur("Joueur " + i + 1 + "-" + k + 1));
+
+			}
+		}
+	}
+
+}
---
+++ Sources/BowlingStub/StubJoueur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class StubJoueur
+{
+private	List<Joueur> listJoueurs = new List<Joueur>();
+	public StubJoueur()
+	{
+	}
+
+	public List<Joueur> ListJoueurs(int n = 10)
+    {
+		for (int i = 0; i < n; i++)
+        {
+				listJoueurs.Add(new Joueur() { Pseudo = "Joueur "+i+1 });
+		}
+    }
+
+}
