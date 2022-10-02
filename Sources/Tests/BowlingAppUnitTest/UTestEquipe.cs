f38aab6e29e56255c19097bf5aab94b584d44e99
AUGUSTIN_100 <augustinaffognon2000@gmail.com>
2022-10-02 13:40:35 +0200

    no message

--- Sources/Tests/BowlingAppUnitTest/UTestEquipe.cs
+++ Sources/Tests/BowlingAppUnitTest/UTestEquipe.cs
@@ -7,31 +7,6 @@ namespace Test.BowlingAppUnitTest
 {
     public class UnitTestEquipe
     {
-        Equipe j = new Equipe("Les rois");
-        [Fact]
-        public void TestConstructeur()
-        {
-            
-
-        [Fact]
-        public void TestInvalidJoueur()
-        {
-            
-        }
-
-        [Theory]
         
-        public void TestContructeur(bool isFormated, bool isValid, string expectedPseudo, String pseudo, bool isEqual)
-        {
-           
-
-        }
-
-        //Test équipe avec stub
-        [Fact]
-        public void TestEquipeStub()
-        {
-            
-        }
     }
 }
