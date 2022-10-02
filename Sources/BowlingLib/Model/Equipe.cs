c4cc18b65c1bad1963b1a8b0d4e242af7cb91d9c
AUGUSTIN_100 <augustinaffognon2000@gmail.com>
2022-10-02 23:12:11 +0200

    Test Unitaire équipe

--- Sources/BowlingLib/Model/Equipe.cs
+++ Sources/BowlingLib/Model/Equipe.cs
@@ -3,8 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+using System.Threading.Tasks; 
 using System.Collections.ObjectModel;
 
 namespace BowlingLib.Model
