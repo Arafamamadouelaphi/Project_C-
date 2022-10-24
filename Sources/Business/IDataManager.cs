9db9b545a2916c09f941bd23823b47d124036426
victor perez ngounou <vngounou26@gmail.com>
2022-10-24 13:11:34 +0200

    ImplementAsyncAwait #63

--- Sources/Business/IDataManager.cs
+++ Sources/Business/IDataManager.cs
@@ -13,10 +13,10 @@ namespace Business
     /// <typeparam name="Data"></typeparam>
     public interface IDataManager<Data>
     {
-        bool Add(Data data);
-        bool Delete(Data data);
-        bool Update(Data data);
-        Data GetDataWithName(string name);
-        IEnumerable<Data> GetAll();
+        Task<bool> Add(Data data);
+        Task<bool> Delete(Data data);
+        Task<bool> Update(Data data);
+        Task<Data> GetDataWithName(string name);
+        Task<IEnumerable<Data>> GetAll();
     }
 }
